99eee35480147d3b295dc1678f038523f9ea4efd
Rohan <rohanahire651@gmail.com>
2024-03-27 14:52:46 +0530

    Fixed the errro
    - Changed the folder name form
      "Providers" to "providers"

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidenav from "@/components/(navigation)/Sidenav";
-import TanstackQueryProvider from "@/Providers/TanstackQueryProvider";
+import TanstackQueryProvider from "@/providers/TanstackQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
